2c5fd4a90edecd2e05f357ea88133e5e95c030ed
souravghoshofficial <souravghosh.tech@gmail.com>
2025-04-03 21:13:05 +0530

    fix nav bar

--- script.js
+++ script.js
@@ -1,17 +1,28 @@
 const hambarger = document.getElementById("hambarger");
-const xmark = document.getElementById("xmark");
 const sidenav = document.getElementById("sidenav");
 const sidenavItem = document.querySelectorAll(".sidenav-item");
+const line1 = document.getElementById("line1");
+const line2 = document.getElementById("line2");
 
 
 hambarger.addEventListener("click", () => {
-    sidenav.style.display = "block";
-    sidenav.style.animation = "sidenavReveal 0.3s linear forwards";
-})
+    if(hambarger.classList.contains("x") ){
+        sidenav.style.animation = "sidenavHide 0.3s linear forwards"
+        line2.style.marginTop = "7px";
+        line1.style.rotate = "0deg";
+        line2.style.rotate = "0deg"
+        hambarger.classList.remove("x")
+    }
+    else{
+        sidenav.style.display = "block";
+        sidenav.style.animation = "sidenavReveal 0.3s linear forwards";
+        line2.style.marginTop = "-2px";
+        line1.style.rotate = "45deg";
+        line2.style.rotate = "-45deg"
+        hambarger.classList.add("x")
+    }
 
-xmark.addEventListener("click", () => {
-    sidenav.style.animation = "sidenavHide 0.3s linear forwards"
-    xmark.style.zIndex = 100;  
+
 })
 
 sidenavItem.forEach(item => (item.addEventListener("click", () => {
